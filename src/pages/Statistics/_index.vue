2d7e2f3a188afc4c04fc3a8e2173341877709dc2
AREEG94FAHAD <fahedareeg@gmail.com>
2020-03-20 13:59:43 -0700

    :tada: call api :bulb:

--- src/pages/Statistics/_index.vue
+++ src/pages/Statistics/_index.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
-        hello world
     </div>
 </template>
+<script>
+    export default {
+        methods:{
+            get_Api(){
+                this.$http.get("https://pomber.github.io/covid19/timeseries.json").then(res=>{
+                    console.log(res.data.Iraq)
+                })
+            }
+        },
+        mounted() {
+           this.test()
+        }
+    }
+</script>
